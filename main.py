d220186a358c19c831121aa8ad7317f09ba8389c
quoc.nguyen <quoc.nguyen@parcelperform.com>
2023-05-23 22:06:32 +0700

    add-in get ads daily

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/get_ads_daily")
+def read_ads_daily():
+    return {
+        "ads_daily_1": "Hello",
+        "ads_daily_2": "World",
+    }
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
